56494349e183b571738719a7b0efc7d5fbdfe483
Rylan0-0 <everydaynormalvlogs@gmail.com>
2024-07-30 16:21:02 -0500

    Fix alignment and sizing issues for login and password inputs

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default Header=>{
     return(
         <div id="header">
-            <h3 style={{color: "#5865F2"}}>Chatter <span style={{color: "#FA8072"}}>Box</span></h3>
+            <h3 style={{color: "#5865F2"}}>Chatter<span style={{color: "#FA8072"}}>Box</span></h3>
         </div>
     );
 }
