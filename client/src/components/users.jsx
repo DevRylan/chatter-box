164712b5c9fc1f9ba30ef7c5e6a5abbe8ddc03f5
Rylan0-0 <everydaynormalvlogs@gmail.com>
2024-08-01 22:29:35 -0500

    Add ability for users to customize room names

--- client/src/components/users.jsx
+++ client/src/components/users.jsx
@@ -4,6 +4,7 @@ import socket from "../socket";
 export default function UserList(props){
     const [currentRoom, setCurrentRoom] = React.useState("");
     const [roomList, setRoomList] = React.useState(["Hub"]);
+    const [customRoom, setCustomRoom] = React.useState("");
 
     function joinRoom(e){
         if (currentRoom && currentRoom != e.target.textContent) socket.emit("leave-room", currentRoom)//Leaves current room user is in
@@ -15,21 +16,23 @@ export default function UserList(props){
     }
 
     function addRoom(){
-        let roomNum = roomList.length;
-        setRoomList(prevValue=>[...prevValue, `Room ${roomNum}`]);//Adds room to array
+        setRoomList(prevValue=>[...prevValue, `${customRoom}`]);//Adds room to array
     }
 
     function Rooms(room, index){
         //Creates room for each room in array
         return(
-            <button onClick={joinRoom} key={index} className="btn btn-dark">
+            <button onClick={joinRoom} key={index} className="btn btn-dark room-button">
                 {room}
             </button>
         );
     }
     return(
     <div id="user-container">
-    <button onClick={addRoom}>Add Room</button>
+    <button className="btn btn-success" onClick={addRoom}>+Create Room</button>
+    <div className="popup">
+        <input type="text" placeholder="Type Here..." onChange={e=>setCustomRoom(e.target.value)} value={customRoom} style={{backgroundColor: "transparent"}}/>
+    </div>
     {roomList.map(Rooms)}
     </div>);
 };
